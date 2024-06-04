6775033ec3705ae0d31024300b5e81c430ed9164
liangzhouzz <l.zhou8332gmail.com>
2024-06-04 22:10:46 +0800

    串口调试

--- build.sh
+++ build.sh
@@ -1,9 +1,36 @@
+# 获取当前脚本文件所在的目录
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 
+if [ ! -d "$SHELL_FOLDER/output" ]; then  
+mkdir $SHELL_FOLDER/output
+fi  
+
+echo "------------------------- 编译qemu---------------------------------------"
 cd qemu-8.0.0
 if [ ! -d "$SHELL_FOLDER/output/qemu" ]; then  
-./configure --prefix=$SHELL_FOLDER/output/qemu --enable-gtk  --enable-virtfs --disable-gio
+./configure --prefix=$SHELL_FOLDER/output/qemu  --target-list=riscv64-softmmu --enable-gtk  --enable-virtfs --disable-gio
 fi  
-make -j16
-sudo make install
-cd ..
+make -j16$PROCESSORS
+make install
+
+
+# # 编译 lowlevelboot
+echo "------------------------- 编译lowlevelboot-----------------------------"
+CROSS_PREFIX=riscv64-unknown-elf
+if [ ! -d "$SHELL_FOLDER/output/lowlevelboot" ]; then  
+mkdir $SHELL_FOLDER/output/lowlevelboot
+fi  
+cd  $SHELL_FOLDER/boot
+# 编译汇编文件startup.s到obj文件
+$CROSS_PREFIX-gcc -x assembler-with-cpp -c start.s -o $SHELL_FOLDER/output/lowlevelboot/start.o
+# 使用链接脚本链接obj文件生成elf可执行文件
+$CROSS_PREFIX-gcc -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/output/lowlevelboot/start.o -o $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf
+# 使用gnu工具生成原始的程序bin文件
+$CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
+# 使用gnu工具生成反汇编文件，方便调试分析（当然我们这个代码太简单，不是很需要）
+$CROSS_PREFIX-objdump --source --demangle --disassemble --reloc --wide $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf > $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.lst
+
+cd $SHELL_FOLDER/output/lowlevelboot
+rm -rf fw.bin
+dd of=fw.bin bs=1k count=32k if=/dev/zero
+dd of=fw.bin bs=1k conv=notrunc seek=0 if=lowlevel_fw.bin
