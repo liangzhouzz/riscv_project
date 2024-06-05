d85fee5eeafa3f12b1139442a62fd0f9e79b750d
liangzhou <l.zhou8332@gmail.com>
2024-06-05 21:14:58 +0800

    riscv64-unknown路径解决

--- build.sh
+++ build.sh
@@ -14,17 +14,17 @@ make -j16$PROCESSORS
 make install
 
 
-# # 编译 lowlevelboot
+# # 编译 lowlevelboot riscv64-unknown-elf riscv64-linux-gnu -L/opt/riscv/riscv64-unknown-elf/lib -L/opt/riscv/lib
 echo "------------------------- 编译lowlevelboot-----------------------------"
 CROSS_PREFIX=riscv64-unknown-elf
 if [ ! -d "$SHELL_FOLDER/output/lowlevelboot" ]; then  
 mkdir $SHELL_FOLDER/output/lowlevelboot
 fi  
-cd  $SHELL_FOLDER/boot
+cd $SHELL_FOLDER/boot
 # 编译汇编文件startup.s到obj文件
-$CROSS_PREFIX-gcc -x assembler-with-cpp -c start.s -o $SHELL_FOLDER/output/lowlevelboot/start.o
+$CROSS_PREFIX-gcc -L/opt/riscv/riscv64-unknown-elf/lib -L/opt/riscv/lib -x assembler-with-cpp -c start.s -o $SHELL_FOLDER/output/lowlevelboot/start.o
 # 使用链接脚本链接obj文件生成elf可执行文件
-$CROSS_PREFIX-gcc -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/output/lowlevelboot/start.o -o $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf
+$CROSS_PREFIX-gcc -L/opt/riscv/riscv64-unknown-elf/lib -L/opt/riscv/lib -nostartfiles -T./boot.lds -Wl,-Map=$SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.map -Wl,--gc-sections $SHELL_FOLDER/output/lowlevelboot/start.o -o $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf
 # 使用gnu工具生成原始的程序bin文件
 $CROSS_PREFIX-objcopy -O binary -S $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.elf $SHELL_FOLDER/output/lowlevelboot/lowlevel_fw.bin
 # 使用gnu工具生成反汇编文件，方便调试分析（当然我们这个代码太简单，不是很需要）
--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ $SHELL_FOLDER/output/qemu/bin/qemu-system-riscv64 \
 -m 1G \
 -smp 8 \
 -bios none \
-#-monitor stdio \
 -drive if=pflash,bus=0,unit=0,format=raw,file=$SHELL_FOLDER/output/lowlevelboot/fw.bin \
--nographic --parallel none
+-monitor stdio \
+#-nographic --parallel none
